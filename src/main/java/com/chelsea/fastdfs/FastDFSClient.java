cbe6ee0bafbc84f84b75cdee7d3e68538c178454
baojun <baojun_a@aspirecn.com>
2018-03-02 01:24:14 +0800

    修改客户端工具类

--- src/main/java/com/chelsea/fastdfs/FastDFSClient.java
+++ src/main/java/com/chelsea/fastdfs/FastDFSClient.java
@@ -1,7 +1,6 @@
 package com.chelsea.fastdfs;
 
 import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.StorageClient1;
 import org.csource.fastdfs.StorageServer;
 import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
@@ -12,7 +11,8 @@ public class FastDFSClient {
     private TrackerServer trackerServer = null;
     private StorageServer storageServer = null;
     // 使用StorageClient1进行上传
-    private StorageClient1 storageClient1 = null;
+    private MyStorageClient1 storageClient1 = null;
+    private static final String GROUP_NAME = "nccgroup1";
 
     public FastDFSClient(String conf) throws Exception {
         // 获取classpath路径下配置文件"fdfs_client.conf"的路径
@@ -23,21 +23,21 @@ public class FastDFSClient {
 
         trackerClient = new TrackerClient();
         trackerServer = trackerClient.getConnection();
-        storageServer = trackerClient.getStoreStorage(trackerServer);
-        storageClient1 = new StorageClient1(trackerServer, storageServer);
+        storageServer = trackerClient.getStoreStorage(trackerServer, GROUP_NAME);
+        System.out.println("ip : " + storageServer.getInetSocketAddress().getAddress().getHostAddress());
+        System.out.println("port : " + storageServer.getSocket().getPort());
+        storageClient1 = new MyStorageClient1(trackerServer, storageServer);
     }
 
     public String uploadFile(byte[] file_buff, String file_ext_name) throws Exception {
-        String result = storageClient1.upload_file1(file_buff, file_ext_name, null);
-        return result;
+        String fileId = storageClient1.upload_file1(file_buff, file_ext_name, null);
+        return fileId;
     }
 
-    public static void main(String[] args) throws Exception {
-        // 不要带classpath
-        FastDFSClient client = new FastDFSClient("fdfs_client.conf");
-        String s = "sadsdad asdadsad asdsadsad[]$";
-        String result = client.uploadFile(s.getBytes(), "txt");
-        System.out.println(result);
+    public byte[] downloadFile(String fileId) throws Exception {
+//        int errorno = storageClient1.download_file1(fileId, "C:/Users/Administrator/Desktop/1.txt");
+//        System.out.println(errorno);
+        return storageClient1.download_file1_http(fileId);
     }
 
 }
